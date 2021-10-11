8879dede8658b015d428e34d00d037fb970d0f80
adhinugroho16 <91260025+adhinugroho16@users.noreply.github.com>
2021-10-11 15:06:25 +0700

    Add files via upload

---
+++ praktikum4_perulangan.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 13:45:49 2021
+
+@author: Surgery Adhi Nugroho
+NIM: 065002100015
+"""
+n = int(input("Masukan angka =  "))
+for i in range (n,0,-1):
+    for j in range (1,i+1):
+        print(i,end='')
+    print()
+        
+    
